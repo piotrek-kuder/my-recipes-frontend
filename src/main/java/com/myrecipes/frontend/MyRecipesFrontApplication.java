29824d52ae385e076b7485ff0cc05cbc7ac35f24
piotrek-kuder <kuder.piotr@o2.pl>
2021-09-18 14:42:03 +0200

    initial commit

---
+++ src/main/java/com/myrecipes/frontend/MyRecipesFrontApplication.java
@@ -0,0 +1,13 @@
+package com.myrecipes.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipesFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRecipesFrontApplication.class, args);
+    }
+
+}
